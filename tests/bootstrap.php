41eb2d517a9225a0ace1e9c531849a15f8e0fb82
Jeremy Mikola <jmikola@gmail.com>
2023-01-20 23:31:01 +0800

    Add PHPUnit dev dep and class alias for older versions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,3 +10,7 @@
 
 $loader = require dirname(__DIR__).'/vendor/autoload.php';
 $loader->add('dflydev\\tests\\util\\antPathMatcher', 'tests');
+
+if (class_exists('PHPUnit\Framework\TestCase') && ! class_exists('PHPUnit_Framework_TestCase')) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
